0eab7437352bf971f672bc6154ba4e7bb6a4cfe2
Kiran Gupta <kiranrambalig@gmail.com>
2026-07-09 09:04:57 +0000

    gc content

--- fasta_read.py
+++ fasta_read.py
@@ -19,3 +19,11 @@ for i in data.splitlines():
         dna.append(i)
         seq[current_line] = "".join(dna)
 print(seq)
+
+gc_content = {}
+for name, sequence in seq.items():
+    try:
+     gc_content[name] = round(((sequence.count("G") + sequence.count("C"))/len(sequence)*100),2)
+    except ZeroDivisionError:
+        print("The length of the sequence is zero.") 
+print(gc_content)
