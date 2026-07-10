bd0a26e3e5126a7a937b970d0a7fbcc1d574e579
Kiran Gupta <kiranrambalig@gmail.com>
2026-07-10 21:50:01 +0530

    cleaned and checked the length of the data

--- mutation/mutation.py
+++ mutation/mutation.py
@@ -1,13 +1,28 @@
 with open("dna.fasta","w") as f:
     f.write(">human\n")
-    f.write("ATGCTGACATA\n")
+    f.write("ATGCTGACATAG\n")
     f.write(">mouse\n")
-    f.write("ATGTTGACAGA")
+    f.write("ATGTTGACAGAT")
 
 with open("dna.fasta","r") as f:
     data = f.read()
-
 print(data)
 
+def clean_data(fasta_seq):
+    cleaned_data = {}
+    for i in fasta_seq.splitlines():
+        if i.startswith(">"):
+            current_seq = i[1:]
+        else:
+            cleaned_data[current_seq] = i
+    return cleaned_data
+dna = clean_data(data)
+print(dna)
 
-    
+def length_of_seq(dna_dict):
+    length = {}
+    for org, seq in dna_dict.items():
+        length[org] = len(seq)
+    return length
+print(length_of_seq(dna))
+
