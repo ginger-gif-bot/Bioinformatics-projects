362f68b11416bd4bb4798b471e06b8b4624523b6
Kiran Gupta <kiranrambalig@gmail.com>
2026-07-12 15:29:23 +0530

    completed till translation of all 6 frames

--- mutation/mutation.py
+++ mutation/mutation.py
@@ -40,21 +40,70 @@ def rev_comp(dna_seq):
         rev_comp_dna = comp_dna[::-1]
     return rev_comp_dna
 
+
 def codon_breakage(dna_dict):
-    dna_codons = {}
+    dna_codons = {"Frame 1":{},
+                  "Frame 2":{},
+                  "Frame 3":{},
+                  "Frame 4":{},
+                  "Frame 5":{},
+                  "Frame 6":{},}
     for org, seq in dna_dict.items():
-        codons_broken = []
-        print(f"the complements are: {rev_comp(seq)}")
+        
         strands = {
             seq:0,
-            rev_comp(seq):3
-        }
-        
-        for base in range(0,len(seq),3):
-         codon = seq[base:base+3]
-         if len(codon) == 3:
+            rev_comp(seq):3}       
+        for all_seq, offset in strands.items():        
+          for frame in range(3):
+           frame_key = f"Frame {frame + 1 + offset}"
+           codons_broken = []
+           for base in range(frame,len(all_seq),3):
+            codon = all_seq[base:base+3]
+            if len(codon) == 3:
              codons_broken.append(codon)
-             dna_codons[org] = codons_broken 
+           dna_codons[frame_key][org] = codons_broken 
     return dna_codons
-
 print(codon_breakage(dna)) 
+
+codon_table = {
+    "TTT":"F","TTC":"F","TTA":"L","TTG":"L",
+    "CTT":"L","CTC":"L","CTA":"L","CTG":"L",
+    "ATT":"I","ATC":"I","ATA":"I","ATG":"M",
+    "GTT":"V","GTC":"V","GTA":"V","GTG":"V",
+    "TCT":"S","TCC":"S","TCA":"S","TCG":"S",
+    "CCT":"P","CCC":"P","CCA":"P","CCG":"P",
+    "ACT":"T","ACC":"T","ACA":"T","ACG":"T",
+    "GCT":"A","GCC":"A","GCA":"A","GCG":"A",
+    "TAT":"Y","TAC":"Y","TAA":"*","TAG":"*",
+    "CAT":"H","CAC":"H","CAA":"Q","CAG":"Q",
+    "AAT":"N","AAC":"N","AAA":"K","AAG":"K",
+    "GAT":"D","GAC":"D","GAA":"E","GAG":"E",
+    "TGT":"C","TGC":"C","TGA":"*","TGG":"W",
+    "CGT":"R","CGC":"R","CGA":"R","CGG":"R",
+    "AGT":"S","AGC":"S","AGA":"R","AGG":"R",
+    "GGT":"G","GGC":"G","GGA":"G","GGG":"G",
+}
+
+def translation(codon_dict):
+   translated_codons = {"Frame 1":{},
+                  "Frame 2":{},
+                  "Frame 3":{},
+                  "Frame 4":{},
+                  "Frame 5":{},
+                  "Frame 6":{},}
+   for frame,info in codon_breakage(dna).items():
+      
+      for org, codons in info.items():   
+         protein = "" 
+         for triplets in codons:
+            aa = codon_table.get(triplets)
+            if aa == None:
+               continue
+            if aa == "*":
+               break
+            protein += aa
+         translated_codons[frame][org] = protein
+   return translated_codons
+print("\nProtein Translation:\n")
+print(translation(codon_breakage(dna)))
+
